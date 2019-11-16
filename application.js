bbc319dd087c35133574746345931d56eda296fa
tikken <big_nick@gmail.com>
2019-11-16 16:46:43 +0300

    initial

---
+++ application.js
@@ -0,0 +1,19 @@
+const db = require('./models/db.js');
+
+const pg = db.open({
+  host: '127.0.0.1',
+  port: 5555,
+  database: 'application',
+  user: 'tikken',
+  password: 'root'
+});
+
+//pg.query();
+pg.select('pg_tables')
+  .where({ tableowner: 'tikken' })
+  .fields(['schemaname', 'tablename', 'tableowner', 'hasindexes'])
+  .order('tablename')
+  .then(rows => {
+    console.table(rows);
+    pg.close();
+});
